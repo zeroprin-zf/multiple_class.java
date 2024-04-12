6a32c6de71740e8503ea4018d38f023a5c6f14db
zeroprin-zf <zerozerozf07@outlook.jp>
2024-04-12 09:49:40 +0900

    計算機プログラム

--- src/multiple_class/java/class_1.java
+++ src/multiple_class/java/class_1.java
@@ -5,6 +5,20 @@ public class class_1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		int a = 10; int b = 2;
+		int total = tasu(a, b);
+		int delta = hiku(a, b);
+		System.out.println("足すと" + total + "、引くと" + delta);
+		
 	}
+	
+	public static int tasu(int a, int b) {
+		return(a + b);
+	}
+	
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+	
 
 }
