74091e0a157bc7f9ea7affd8853b3564b7ef2fa9
zeroprin-zf <zerozerozf07@outlook.jp>
2024-04-13 10:19:43 +0900

    クラスを分割した

--- src/multiple_class/java/Calc.java
+++ src/multiple_class/java/Calc.java
@@ -1,24 +1,14 @@
 package multiple_class.java;
 
-public class class_1 {
+public class Calc {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
 		int a = 10; int b = 2;
-		int total = tasu(a, b);
-		int delta = hiku(a, b);
+		int total = CalcLogic.tasu(a, b);
+		int delta = CalcLogic.hiku(a, b);
 		System.out.println("足すと" + total + "、引くと" + delta);
-		
 	}
-	
-	public static int tasu(int a, int b) {
-		return(a + b);
-	}
-	
-	public static int hiku(int a, int b) {
-		return (a - b);
-	}
-	
 
 }
---
+++ src/multiple_class/java/CalcLogic.java
@@ -0,0 +1,11 @@
+package multiple_class.java;
+
+public class CalcLogic {
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+	public static int hiku(int a, int b) {
+		return (a + b);
+	}
+
+}
